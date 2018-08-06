53f2463595e3d6c0b5a2252ee6c8fc04e2e5f1cf
Rusz0901 <russel090186@gmail.com>
2018-08-06 20:38:44 +0800

    this is my first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+
+
+var config = 
+{
+    database: 
+    {
+        host:       'localhost',     // database host
+        user:       'root',         // your database username
+        password:   '',         // your database password
+        port:        3306,         // default MySQL port
+        db:         'questronix'         // your database name
+    },
+    server: 
+    {
+        host: '127.0.0.1',
+        port: '3000'
+    }
+}
+ 
+module.exports = config
